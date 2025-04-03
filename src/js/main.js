5174a175ff5736aa632342eb2c71c6e79044fb86
Huzaifahdv <holmzsharlok66@gmail.com>
2025-04-04 00:06:55 +0600

    set up webpack

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import '../sass/style.scss';
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: "./src/js/main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [require("postcss-preset-env")]
+                            }
+                        }
+                    },
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/images/[name][ext]",
+                },
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            sources: {
+                                list: [
+                                    {
+                                        tag: "img",
+                                        attribute: "src",
+                                        type: "src",
+                                    },
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [`...`, new CssMinimizerPlugin()]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({ filename: "styles.css" }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 9000,
+        devMiddleware: {
+            writeToDisk: (filePath) => {
+                return !/hot-update/i.test(filePath);
+              },
+        },
+        hot: true,
+        open: true,
+    }
+};
